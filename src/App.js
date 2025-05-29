959d42bfdb9c1487690a10be952b5d2c6bbd7df3
Ankit Raj <118665496+ankitrpathak@users.noreply.github.com>
2025-05-29 15:47:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -33,14 +33,14 @@ export default class App extends Component {
           />
           
           <Routes>
-            <Route exact path="/" element={<News setProgress={this.setProgress} apiKey=cf163fb4e990465bb709823c15905976 key="general" pageSize={this.pageSize} country="in" category="general"/>}/>
-            <Route exact path="/general" element={<News setProgress={this.setProgress} apiKey=cf163fb4e990465bb709823c15905976 key="general" pageSize={this.pageSize} country="in" category="general"/>}/>
-            <Route exact path="/business" element={<News setProgress={this.setProgress} apiKey=cf163fb4e990465bb709823c15905976 key="business" pageSize={this.pageSize} country="in" category="business"/>}/>
-            <Route exact path="/entertainment" element={<News setProgress={this.setProgress} apiKey=cf163fb4e990465bb709823c15905976 key="entertainment" pageSize={this.pageSize} country="in" category="entertainment"/>}/>
-            <Route exact path="/health" element={<News setProgress={this.setProgress} apiKey=cf163fb4e990465bb709823c15905976 key="health" pageSize={this.pageSize} country="in" category="health"/>}/>
-            <Route exact path="/science" element={<News setProgress={this.setProgress} apiKey=cf163fb4e990465bb709823c15905976 key="science" pageSize={this.pageSize} country="in" category="science"/>}/>
-            <Route exact path="/sports" element={<News setProgress={this.setProgress} apiKey=cf163fb4e990465bb709823c15905976 key="sports" pageSize={this.pageSize} country="in" category="sports"/>}/>
-            <Route exact path="/technology" element={<News setProgress={this.setProgress} apiKey=cf163fb4e990465bb709823c15905976 key="technology" pageSize={this.pageSize} country="in" category="technology"/>}/>
+            <Route exact path="/" element={<News setProgress={this.setProgress} apiKey="cf163fb4e990465bb709823c15905976" key="general" pageSize={this.pageSize} country="in" category="general"/>}/>
+            <Route exact path="/general" element={<News setProgress={this.setProgress} apiKey="cf163fb4e990465bb709823c15905976" key="general" pageSize={this.pageSize} country="in" category="general"/>}/>
+            <Route exact path="/business" element={<News setProgress={this.setProgress} apiKey="cf163fb4e990465bb709823c15905976" key="business" pageSize={this.pageSize} country="in" category="business"/>}/>
+            <Route exact path="/entertainment" element={<News setProgress={this.setProgress} apiKey="cf163fb4e990465bb709823c15905976" key="entertainment" pageSize={this.pageSize} country="in" category="entertainment"/>}/>
+            <Route exact path="/health" element={<News setProgress={this.setProgress} apiKey="cf163fb4e990465bb709823c15905976" key="health" pageSize={this.pageSize} country="in" category="health"/>}/>
+            <Route exact path="/science" element={<News setProgress={this.setProgress} apiKey="cf163fb4e990465bb709823c15905976" key="science" pageSize={this.pageSize} country="in" category="science"/>}/>
+            <Route exact path="/sports" element={<News setProgress={this.setProgress} apiKey="cf163fb4e990465bb709823c15905976" key="sports" pageSize={this.pageSize} country="in" category="sports"/>}/>
+            <Route exact path="/technology" element={<News setProgress={this.setProgress} apiKey="cf163fb4e990465bb709823c15905976" key="technology" pageSize={this.pageSize} country="in" category="technology"/>}/>
           </Routes>
         </Router>
       </div>
